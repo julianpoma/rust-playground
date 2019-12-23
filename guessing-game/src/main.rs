b72ee5515a9ba1be550bab8e040a2103392a7a6a
Julian Poma <poma.nob@gmail.com>
2019-12-23 14:28:41 -0300

    Guess game finished

--- guessing-game/src/main.rs
+++ guessing-game/src/main.rs
@@ -4,26 +4,37 @@
 extern crate rand;
 
 use rand::Rng;
+use std::cmp::Ordering;
 use std::io;
 
 fn main() {
-    println!("Welcome! Enter your guess:");
+    let random: u8 = rand::thread_rng().gen_range(1, 101);
+    
+    loop {
+        println!("Enter your guess:");
 
-    let mut guess = String::with_capacity(100);
+        let mut guess = String::with_capacity(100);
+        
+        io::stdin()
+            .read_line(&mut guess)
+            .expect("Input is required");
 
-    let random: u32 = rand::thread_rng().gen_range(1, 101);
+        // Shadowing
+        let guess: u8 = match guess.trim().parse() {
+            Ok(num) => num,
+            Err(_) => {
+                println!("Please enter a number");
+                continue;
+            }
+        };
 
-    io::stdin()
-        .read_line(&mut guess)
-        .expect("Input is required");
-
-    let trim_guess = guess.trim();
-
-    let int_guess: u32 = trim_guess.parse().unwrap();
-
-    if int_guess == random {
-        println!("Your guess {} was correct!", guess);
-    } else {
-        println!("Your guess {} was incorrect: {}", guess, random);
+        match guess.cmp(&random) {
+            Ordering::Less => println!("To small"),
+            Ordering::Equal => {
+                println!("You won!");
+                break;
+            }
+            Ordering::Greater => println!("To big"),
+        }
     }
 }
