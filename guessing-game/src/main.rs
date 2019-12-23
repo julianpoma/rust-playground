b1cca0de91480f825909d410ed78bf39d6c5d4a3
Julian Poma <poma.nob@gmail.com>
2019-12-23 18:56:12 -0300

    Check number range

--- guessing-game/src/main.rs
+++ guessing-game/src/main.rs
@@ -8,25 +8,28 @@ use std::cmp::Ordering;
 use std::io;
 
 fn main() {
-    let random: u8 = rand::thread_rng().gen_range(1, 101);
-    
+    let random: u32 = rand::thread_rng().gen_range(1, 101);
     loop {
         println!("Enter your guess:");
 
         let mut guess = String::with_capacity(100);
-        
         io::stdin()
             .read_line(&mut guess)
             .expect("Input is required");
 
         // Shadowing
-        let guess: u8 = match guess.trim().parse() {
+        let guess: u32 = match guess.trim().parse() {
             Ok(num) => num,
             Err(_) => {
                 println!("Please enter a number");
                 continue;
             }
         };
+
+        if guess < 1 || guess > 100 {
+            println!("Enter a number between 1 and 100");
+            continue;
+        }
 
         match guess.cmp(&random) {
             Ordering::Less => println!("To small"),
