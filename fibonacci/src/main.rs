83bf7516105b496d2982ae229d926b4408e97334
Julian Poma <poma.nob@gmail.com>
2019-12-23 21:32:29 -0300

    WIP

--- fibonacci/src/main.rs
+++ fibonacci/src/main.rs
@@ -3,24 +3,30 @@ use std::io;
 fn main() {
     println!("Enter the amount of iterations");
 
-    let mut iterations = String::with_capacity(100);
+    loop {
+        let mut iterations = String::with_capacity(100);
 
-    io::stdin().read_line(&mut iterations).expect("Please enter a valid input");
+        io::stdin()
+            .read_line(&mut iterations)
+            .expect("Error reading input");
 
-    let iterations: u32 = match iterations.trim().parse() {
-        Ok(num) => num,
-        Err(_) => {
-            panic!("Please enter a valid input")
-        },
-    };
+        let iterations: u32 = match iterations.trim().parse() {
+            Ok(num) => num,
+            Err(_) => {
+                println!("Please enter a valid input");
+                continue;
+            }
+        };
 
-    for number in 0..iterations + 1 {
-        let x = fibonacci(number);
-        println!("{}", x);
+        for number in 0..iterations + 1 {
+            println!("{}", fibonacci(number));
+        }
+
+        break;
     }
 }
 
-fn fibonacci (number: u32) -> u32 {
+fn fibonacci(number: u32) -> u32 {
     if number == 0 {
         return 0;
     }
