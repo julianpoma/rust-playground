13a771d1aa63e123332eb880abe57affa6956a90
Julian Poma <poma.nob@gmail.com>
2020-01-21 11:15:19 -0300

    Improvments

--- modules/src/main.rs
+++ modules/src/main.rs
@@ -2,10 +2,16 @@ mod util;
 mod helper;
 
 use helper::helper_utils;
+// If we use this, we can call helper utils functions like this
+// helper_utils::helper_caller();
 
 fn main() {
     util::util_caller();
-    helper_utils::helper_caller();
+    
+    // Two ways of calling the helper_utils functions
+    helper::helper_utils::helper_caller();
+    // We need the use above to do this
     helper_utils::deep_caller();
+
     helper::mod_main();
 }
